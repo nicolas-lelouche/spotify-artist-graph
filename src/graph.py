0f84a9f9b268209538a659f29727e1cfb87af0d0
Nicolas Lelouche <nicolas.lelouche@nist.gov>
2024-04-17 14:31:05 -0400

    adding node-size scaling according to number of songs on Spotify, and edge width according to number of collaborations

--- src/graph.py
+++ src/graph.py
@@ -7,12 +7,22 @@ artist_songs, number_of_artist_appearance = songs.get_songs("test/data/artist-li
 
 G = nx.Graph()
 
+print(number_of_artist_appearance.keys())
+
 for song in artist_songs:
-    if len(artist_songs[song]) > 1:
-        G.add_edge(artist_songs[song][0], artist_songs[song][1])
+    number_of_artists_featured = len(artist_songs[song])
+    for i in range(number_of_artists_featured - 1):
+        for j in range(i + 1, number_of_artists_featured):
+            if G.has_edge(artist_songs[song][i], artist_songs[song][j]):
+                G[artist_songs[song][i]][artist_songs[song][j]]['weight'] += 1
+            else:
+                G.add_edge(artist_songs[song][i], artist_songs[song][j], weight = 1)
 
-d = nx.degree(G)
 
 
-nx.draw(G, nodelist=d.keys(), node_size=[v * 100 for v in d.values()])
+edges = G.edges()
+weights = [G[u][v]['weight'] for u,v in edges]
+
+nx.draw_networkx(G, nodelist = number_of_artist_appearance.keys(), node_size = [v * 100 for v in number_of_artist_appearance.values()],
+                    width = weights) 
 plt.show() 
